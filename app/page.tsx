ebafaff7e9d0f28c2b1d7129a35c3bed1d2ca421
Rafiq Hilali <rafiq@lambertlabs.com>
2025-09-27 14:10:31 +0100

    add ph badge

--- app/page.tsx
+++ app/page.tsx
@@ -42,9 +42,10 @@ const page = () => {
                     Open the app
                 </HoverBorderGradient>
             </Link>
-
-            <a href="https://www.producthunt.com/products/textbehindvideo-io?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-textbehindvideo&#0045;io" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1020076&theme=light&t=1758818861334" alt="textbehindvideo&#0046;io - Create&#0032;cinematic&#0032;text&#0032;behind&#0032;video&#0032;designs&#0032;easily | Product Hunt" style={{width: 250, height: 54}} width="250" height="54" /></a>
-            
+            <div className='flex flex-col gap-2 sm:flex-row'>
+                <a href="https://www.producthunt.com/products/textbehindvideo-io?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-textbehindvideo&#0045;io" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1020076&theme=light&t=1758818861334" alt="textbehindvideo&#0046;io - Create&#0032;cinematic&#0032;text&#0032;behind&#0032;video&#0032;designs&#0032;easily | Product Hunt" style={{width: 225, height: 49}} width="225" height="49" /></a>
+                <a href="https://www.producthunt.com/products/textbehindvideo-io?embed=true&utm_source=badge-top-post-badge&utm_medium=badge&utm_source=badge-textbehindvideo&#0045;io" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=1020076&theme=light&period=daily&t=1758977543075" alt="textbehindvideo&#0046;io - Create&#0032;cinematic&#0032;text&#0032;behind&#0032;video&#0032;designs&#0032;easily | Product Hunt" style={{width: 225, height: 49}} width="225" height="49" /></a>
+            </div>
             <div className='w-full h-full mt-2'>
                 <HeroVideoGallery />
             </div>
